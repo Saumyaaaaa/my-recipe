05d0c1616618d74c3b482b5bb5fab0e175eebc6c
WooFoo <sujanstha3220@gmail.com>
2023-10-18 15:39:04 +0545

    file extension is changed from js to jSX

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+export default function Home() {
+	return (
+		<>
+			<div className="h-screen w-full homepage">
+				<div className="w-4/5 absolute top-1/2 left-1/2 -translate-x-1/2  -translate-y-1/2 text-center">
+					<h1 className="my-8 text-5xl">
+						Explore food from around the world
+					</h1>
+					<Link
+						className="shadow-gray-50 bg-gray-300 rounded text-xl px-2 py-2 cursor-pointer duration-200 hover:bg-red-700 hover:text-white"
+						href="/types"
+					>
+						List of cuisines
+					</Link>
+				</div>
+			</div>
+		</>
+	);
+}
